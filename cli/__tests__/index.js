cb5560a007b8260daad79709716c9b769e51062e
Chernobrov Mike <m.chernobrov@rambler-co.ru>
2016-11-17 16:01:38 +0300

    commit for travis CI check

--- cli/__tests__/index.js
+++ cli/__tests__/index.js
@@ -3,6 +3,16 @@
 const { assert } = require('chai');
 const { descript } = require('mocha');
 
+
+describe('ship run build', function() {
+
+});
+
+describe('ship run dev', function() {
+
+});
+
 describe('commands.utils', function() {
 
 });
+
