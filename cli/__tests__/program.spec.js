72de9ae0f00aa8e1fee52784f8c9597d07378ea8
Chernobrov Mike <m.chernobrov@rambler-co.ru>
2016-11-18 16:34:43 +0300

    integrate code climate

---
+++ cli/__tests__/program.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const CLI = require('../index')();
+const { expect } = require('chai');
+const { describe } = require('mocha');
+
+describe('formal CLI tests', () => {
+
+  it('CLI should be an object', () => {
+    expect(CLI).to.be.a('object');
+  });
+
+  it('CLI.commands should be an array of commands', () => {
+    expect(CLI.commands).to.be.a('array');
+  });
+
+  // console.log(API.commands);
+
+});
